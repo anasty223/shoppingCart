081521d452c027ae792ff00f49e1b8a9f1644ec5
Anastasya <anasty22@gmail.com>
2023-01-21 20:36:17 +0200

    add style

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
 import './App.css'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Cart from './components/Cart'
+import ProductsList from './components/ProductsList'
+import { useState } from 'react'
 
 function App() {
+const [viewCart,setViewCart]=useState<boolean>(false)
 
+const pageContent=viewCart?<Cart/>:<ProductsList/>
 
-  return (
-    <div className="App">
- 
-    </div>
-  )
+const content=(
+  <>
+  <Header viewCart={viewCart} setViewCart={setViewCart}/>
+  {pageContent}
+  <Footer viewCart={viewCart}/>
+  </>
+)
+
+  return content
+   
 }
 
 export default App
